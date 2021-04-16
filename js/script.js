57944753d9208e0f62ddd7a821ed45a2713bd82e
Benny <bbxblack@gmail.com>
2021-04-16 17:44:14 +0200

    v0.1

--- js/script.js
+++ js/script.js
@@ -1,17 +1,3 @@
-// attraverso vue&axios scaricare 10 numeri interi e produrre le box
-// corrispondenti, colorandone il bg verde nel caso di numeri pari
-// e rossi nel caso di numeri dispari
-
-
-
-
-// chiedere il numero di box da generare all'utente attraverso un prompt
-// e generare anche una box rappresentante la somma dei valori ricevuti
-
-
-
-
-
 // Attraverso una chiamata ajax all'API di boolean
 // https://flynn.boolean.careers/exercises/api/array/music
 // avremo a disposizione una decina di dischi musicali.
@@ -23,52 +9,35 @@
 // BONUS 2: Ordinare i dischi per anno di uscita.
 
 
-function boxNumberGenerator() {
-  // AXIOS
+function diskGenerator() {
+
   new Vue({
-        el: '#app',
-        data: {
 
-        },
-        mounted() {
-            axios.get('https://flynn.boolean.careers/exercises/api/array/music',
-                })
-                .then(data => {
-                    const values = data.data.response;
-                    for (let i=0;i<values.length;i++) {
-                        const value = values[i];
-                        const isPair = value % 2 == 0;
-                        this.values.push({
-                            value,
-                            class: isPair ? 'bg-green' : 'bg-red'
-                        });
-                    }
-                })
-                .catch(() => console.log('error'));
-        }
-    });
-}
-  }
+    el: '#app',
+    data:{
+      arrArtists: []
+    },
+    mounted() {
+      axios.get('https://flynn.boolean.careers/exercises/api/array/music')
+      .then(
+        preInfo =>
+        {console.log(preInfo);
 
-  function stampBoxes(num) {
-
-    const target = $('#list');
-
-    if (num % 2 == 0) {
-
-      target.append('<li class="box bg-green">' + num + '</li>');
-
-    } else {
-
-      target.append('<li class="box bg-red">' + num + '</li>')
-
+          artists = preInfo.data.response;
+          arrArtists.push(artists);
+          console.log(arrArtists);
+      })
+      .catch(() => console.log(' error'))
     }
 
-  }
+  });
+
+}
 
 function init() {
 
-  }
+  diskGenerator();
 
 }
+
 document.addEventListener('DOMContentLoaded', init);
